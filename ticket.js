f27c0f11c6c17aa627c1e0183edfa1d56febc9a2
G3org3B <vbt.george@gmail.com>
2018-11-13 17:42:41 +0100

    complete ex

---
+++ ticket.js
@@ -0,0 +1,32 @@
+
+// richiesta utente - numero km e età //
+
+var age = prompt("Quanti anni hai?");
+
+var km = prompt("Quanti km devi percorrere?");
+
+var ticket = 0.21; //priceticket 1km//
+
+// calcolo prezzo base del viaggio //
+
+var totalticket = km * ticket;
+console.log(km * ticket);
+
+// Applicazione degli sconti //
+
+// sconto 20% //
+if (age <= 18) {
+  ticketsale = ((ticket / 100) * 80) * km;
+  console.log((ticket / 100) * 80) * km;
+
+}
+// sconto 40% //
+else if (age >= 64) {
+  ticketsale = ((ticket / 100) * 60) * km;
+  console.log((ticket / 100) * 60) * km;
+
+}
+
+// prezzo scontato //
+var finalticket = ticketsale * km;
+document.getElementById("finalticket").innerHTML = ("Il suo prezzo totale compreso di sconto è" + finalticket)
